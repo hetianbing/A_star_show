c1994acce8acc3dbbb499bd9a9cebfa1f5278784
hetianbing <72117132+hetianbing@users.noreply.github.com>
2022-06-02 23:36:22 +0800

    Update main.py
    增加一些注释

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import pygame
 import sys
 from pygame.sprite import Group
-
-
 import gamefunction as gf
 from setting import Setting
 from node_data import Node_Data
-
 from time import sleep
 from time import time
-
 from Button import AS_BUTTON
 
 def main():
@@ -31,27 +27,24 @@ def main():
     #按钮列表
     button_list = [AS_BUTTON(as_setting, screen, msg) for msg in ["SET_START", "SET_END", "SET_BLOCK", "SET_BLANK"]]
 
+    # 后面依次是：运行（单步）、自动运行、清除所有节点、类型（分为直线型，斜线型，加强斜线型（对角线代价为1.4））
     step_run = AS_BUTTON(as_setting, screen, "RUN")
     auto_run = AS_BUTTON(as_setting, screen, "AUTO")
     clear_all = AS_BUTTON(as_setting, screen, "CLEAR")
     change_type= AS_BUTTON(as_setting, screen, "STRAIGHT") # slanted
 
-
     while True:
-
-        # node_arr.draw_nodes()
         #更新结果
         gf.update_screen(as_setting, screen, node_arr, button_list, step_run, auto_run, clear_all, change_type)
 
         #响应行为
         while not gf.check_event(as_setting, screen, node_arr, button_list, step_run, auto_run, clear_all, change_type):
             if as_setting.is_auto_run and not node_arr.is_find_way and time() >= as_setting.last_time + as_setting.delay_time:
+                # 如果自动运行，那么就不等待事件响应了
                 node_arr.run_once()
                 as_setting.last_time = time()
                 break
             sleep(0.005)
-
-
 
 
 
